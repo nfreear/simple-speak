25a83207f23213d73b3e49681bbd8a9110401113
Nick Freear <nfreear@yahoo.co.uk>
2017-07-04 22:28:59 +0100

    Bug #2, Add `whenDeferred()` - weather bridge Javascript [iet:9087033]

--- src/bridge/weather.js
+++ src/bridge/weather.js
@@ -7,41 +7,71 @@
 (function (W) {
   'use strict';
 
+  var $ = W.jQuery;
   var $widget = $('.aw-widget-current');
-  var simpleSpeakJs = 'https://unpkg.com/simple-speak@1.3.0-beta#._.js';
+  var script = 'https://unpkg.co/simple-speak@1.3.0-beta#._.js';
   var sronlyTpl = '<i class="sr-only"> %s </i>';
 
-  $('a.aw-widget-legal').attr('aria-label', 'Accu-weather forecast.')
+  var id = $widget.attr('id');
+  var config = {
+    id: id + '-inner',
+    lang: $widget.data('language')
+  };
 
-  W.setTimeout(function () {
-    var $inner = $widget.find('.aw-widget-content'); //find('.aw-widget-current-inner');
-    var id = $widget.attr('id');
-    var config = {
-      id: id + '-inner',
-      lang: $widget.data('language')
-    };
+  whenDeferred(function () {
+    return $widget.find('.aw-widget-content').length;
+  })
+  .fail(function () {
+    console.error('Weather: fail.');
+  })
+  .then(function () {
+    var $inner = $widget.find('.aw-widget-content');
+
+    addScriptConfig(script, config);
 
     $inner.attr('id', id + '-inner');
 
+    $widget.addClass('weather-speak-js');
+
     console.warn('Weather, raw: ', $inner.text(), $widget.data('language'));
 
-    $widget.attr('data-simple-speak', JSON.stringify(config));
+    $('a.aw-widget-legal').attr('aria-label', 'Accu-weather forecast.');
 
     inject('h3', 'Weather forecast.', 'before');
 
     inject('h3', '. Location:');
     inject('.aw-temperature-today', '. Temperature:');
-    inject('.aw-temperature-today', 'celcius.', 'append');
-    // $widget.find('h3').prepend(sronlyTpl.replace(/%s/, 'Location:'));
-    // $widget.find('.aw-temperature-today').prepend(sronlyTpl.replace(/%s/, '. Temperature:'));
-    // $widget.find('.aw-temperature-today').append(sronlyTpl.replace(/%s/, 'celcius.'));
-    $widget.find('.aw-weather-description').prepend(sronlyTpl.replace(/%s/, 'Conditions:'));
+    inject('.aw-temperature-today', (isCelcius ? 'celcius.' : 'fahrenheit.'), 'append');
+    inject('time', '. Time:', 'before');
+    inject('.aw-weather-description', '. Conditions:');
+  });
 
+  function addScriptConfig (script, config) {
     $.ajaxSetup({ cache: true }); // Remove the '?_=..' timestamp parameter.
 
-    $('body').append('<script src="%s">'.replace(/%s/, simpleSpeakJs));
-  },
-  3000);
+    $('body').append('<script data-simple-speak=\'%c\' src="%s">'
+      .replace(/%c/, JSON.stringify(config))
+      .replace(/%s/, script)
+    );
+  }
+
+  // https://gist.github.com/nfreear/f40470e1aec63f442a8a
+  function whenDeferred (whenTrueFn, args, /* callbackFn, */ interval, limit) {
+    var deferred = $.Deferred();
+    var intId = W.setInterval(function () {
+      if (whenTrueFn()) {
+        W.clearInterval(intId);
+        deferred.resolve(args); // callbackFn();
+      }
+    }, interval || 300); // Milliseconds.
+
+    W.setTimeout(function () {
+      W.clearInterval(intId);
+      deferred.reject();
+    }, limit || 4000);
+
+    return deferred;
+  }
 
   function isCelcius () {
     return $widget.data('unit') === 'c';
