e87bb2b59564950f2656276ab03a9f6b2b77f1e9
Nick Freear <nfreear@yahoo.co.uk>
2018-01-23 20:08:22 +0000

    Fix to `configure-test` Javascript [iet:9087033]

--- test/configure-test.js
+++ test/configure-test.js
@@ -1,22 +1,44 @@
 /*!
   configure-test | © Nick Freear, 30-May-2017.
+
+  https://github.com/tmpvar/jsdom/tree/9.12.0
+  https://stackoverflow.com/questions/31873604/write-browserify-output-to-variable
 */
 
 const jsdom = require('jsdom');
 // const { JSDOM } = jsdom;
+const browserify = require('browserify');
+const fs = require('fs');
+const path = require('path');
+const jquery = fs.readFileSync(path.join(__dirname, '/../node_modules/jquery/dist/jquery.min.js'), 'utf-8');
 
-jsdom.env('', function (err, window) {
+browserify(path.join(__dirname, '/../src/configure.js')).bundle();
+// console.log(js);
+// return;
+
+var vc = jsdom.createVirtualConsole().sendTo(console);
+
+jsdom.env('html/configure-test.html', { src: [ jquery ], virtualConsole: vc }, function (err, window) {
+// dom.fromFile('html/configure-test.html').then(function (err, window) {
+// jsdom.env('', function (err, window) {
+
   if (err) {
     console.error(err);
     return;
   }
 
+  var $ = window.jQuery;
+
+  $(function ($) {
+    console.log('TITLE: ' + $('title').text());
+
+    var config = require(path.join(__dirname, '/../src/configure.js')).configure(window);
+
+    console.log('simple-speak: ');
+    console.dir(config);
+  });
+
   // var $ = require('jquery')(window);
 
-  var config = require('../src/configure.js').configure(window);
-
-  require('../src/compat.js').compatible(config, window);
-
-  console.log('simple-speak: ');
-  console.dir(config);
+  // require('../src/compat.js').compatible(config, window);
 });
